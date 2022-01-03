236d9e829acabe7bb827b0761295ad539eb610b7
yessense <Akorchemnyj@gmail.com>
2022-01-03 14:42:46 +0300

    arg_bindings is ready

--- arg_binding/arg_binding.py
+++ arg_binding/arg_binding.py
@@ -1,8 +1,10 @@
 import types
 from collections import OrderedDict
 from types import FunctionType
-from typing import Any, Dict, Optional, Tuple, List
+from typing import Any, Dict, Optional, Tuple, List, cast
 from dis import COMPILER_FLAG_NAMES as compiler_flag_names
+
+import pytest
 
 COMPILER_FLAG_NAMES = {1: 'OPTIMIZED',
                        2: 'NEWLOCALS',
@@ -62,69 +64,68 @@ def bind_args(func: FunctionType, *args: Any, **kwargs: Any) -> Dict[str, Any]:
 
     call_args: Dict[str, Any] = {}
     f_kwargs: Dict[str, Any] = {}
-
-    # ----------------------------------------
-    # POS ARGS
-    # ----------------------------------------
-    # number of default elements
-    n_defaults = len(defaults) if defaults is not None else 0
-    n_args = len(args)
-
-    if n_defaults + n_args < co_arg_count:
-        raise TypeError(ERR_MISSING_POS_ARGS)
-
-    if n_args > co_arg_count:
-        if (co_flags & CO_VARARGS):
-            if (co_flags & CO_VARKEYWORDS):
-                kw_name = co_varnames[-1]
-                call_args[kw_name] = f_kwargs
-                args_name_pos = -2
-            else:
-                args_name_pos = -1
-            name = co_varnames[args_name_pos]
-            call_args[name] = args[n_args - co_arg_count:]
-        else:
-            raise TypeError(ERR_TOO_MANY_POS_ARGS)
-
-    defaults_pos = 0
-    for i in range(co_arg_count):
-        name = co_varnames[i]
-        if i <= n_args:
-            call_args[name] = args[i]
-        else:
-            call_args[name] = defaults[defaults_pos]
-            defaults_pos += 1
+    f_varargs: Tuple[Any] = tuple()
 
     # ----------------------------------------
     # KW ARGS
     # ----------------------------------------
 
-    for i in range(co_kwonlyargcount):
-        name = co_varnames[co_arg_count + i]
-        if name in kwargs:
-            call_args[name] = kwargs[name]
-        elif name in kwdefaults:
-            call_args[name] = kwdefaults[name]
-        else:
-            raise TypeError(ERR_MISSING_KWONLY_ARGS)
-
-    for key, value in kwargs:
-        if key not in co_varnames and (CO_VARKEYWORDS & co_flags):
+    for key, value in kwargs.items():
+        if key in co_varnames:
+            call_args[key] = value
+        elif co_flags & CO_VARKEYWORDS:
             f_kwargs[key] = value
         else:
             raise TypeError(ERR_TOO_MANY_KW_ARGS)
 
+    # ----------------------------------------
+    # POS ARGS
+    # ----------------------------------------
+
+    # number of default elements
+    n_defaults = len(defaults) if defaults is not None else 0
+    n_args = len(args)
+    defaults_pos = co_arg_count - n_defaults
+
+    for i, name in zip(range(co_arg_count), co_varnames):
+        if i < n_args:
+            call_args[name] = args[i]
+            if name in kwargs:
+                raise TypeError(ERR_MULT_VALUES_FOR_ARG)
+        elif name in kwargs:
+            call_args[name] = kwargs[name]
+        elif n_defaults and i - defaults_pos < n_defaults and i - defaults_pos >= 0:
+            call_args[name] = defaults[i - defaults_pos]
+        else:
+            raise TypeError(ERR_MISSING_POS_ARGS)
+
+    if n_args > co_arg_count:
+        if co_flags & CO_VARARGS:
+            f_varargs = args[co_arg_count:]
+        else:
+            raise TypeError(ERR_TOO_MANY_POS_ARGS)
+
+    kwonly_pos = co_arg_count
+    for i in range(co_kwonlyargcount):
+        name = co_varnames[kwonly_pos + i]
+        if (name not in kwargs and
+                (kwdefaults is None or
+                 name not in kwdefaults)):
+            raise TypeError(ERR_MISSING_KWONLY_ARGS)
+        elif (kwdefaults is not None and
+              name in kwdefaults and
+              name not in call_args):
+            call_args[name] = kwdefaults[name]
+
+    args_name_pos = -1
+
+    if co_flags & CO_VARKEYWORDS:
+        kwargs_name = co_varnames[-1]
+        call_args[kwargs_name] = f_kwargs
+        args_name_pos -= 1
+
+    if co_flags & CO_VARARGS:
+        args_name = co_varnames[args_name_pos]
+        call_args[args_name] = f_varargs
+
     return call_args
-
-
-def my_func(a, b=2, c=1, d=None, *args, **kwargs):
-    # print(a)
-    # print(b)
-    # print(c)
-    # print(args)
-    # print(kwargs)
-    return None
-
-
-if __name__ == '__main__':
-    bind_args(my_func, )
